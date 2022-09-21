0e2b0c519e212e6391acc3427185c24f8e39b0b5
Clement Azalbert <clementazalbert04@gmail.com>
2022-09-21 12:37:03 +0200

    added search functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -71,6 +71,24 @@ export class AppComponent implements OnInit {
     });
   }
 
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    // Check for clean code solution to refactor this piece of code
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+
   public onOpenModal(employee: Employee | null, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
